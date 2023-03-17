193944e115786430cd6c300eb63af82262593482
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-03-17 15:27:41 -0300

    arquivos novos

---
+++ JS/aula3/index.js
@@ -0,0 +1,7 @@
+console.log('Rafael "X Simões"'); //string = texto
+console.log("'Rafael' X Simões"); // Number = Número
+console.log(`'Rafael' "X Simões"`);
+
+console.log(35, 15.85, 'rafael xsimões');
+
+console.log('Meu nome é "NOME". Estou apresendendo JavaScript às',10 ,'da manhã.')
