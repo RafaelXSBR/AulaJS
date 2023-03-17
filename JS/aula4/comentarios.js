21a6b440ab518fad590d8099dcd8424fb3d83b78
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-03-17 15:42:56 -0300

    arq novo

---
+++ JS/aula4/comentarios.js
@@ -0,0 +1,16 @@
+// E escrever um comentário
+console.log('óla mundo'); // Aqui tem outro comentário
+console.log('linha 1');
+// Comentário
+
+/* 
+* Um titulo
+*
+* Lorem ipsum
+*/
+//console.log('linha 2')
+/*
+console.log('linha 3')
+console.log('linha 4')
+*/
+//vai ser ignorado
