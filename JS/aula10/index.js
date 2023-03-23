cb33627191310b2d9f26ef2aec1c0f0f359e2529
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-03-23 18:53:07 -0300

    add

---
+++ JS/aula10/index.js
@@ -0,0 +1,34 @@
+/** 
+ * Aritméticos 
+*+ adição / concatenação ( - / * )
+*% Resto da divisão
+()
+**
+* / %
++ -
+*/
+
+const num1 =5;
+const num2 = 2;
+const num3 = 10;
+console.log(num1 ** num2);
+console.log(num1 % num2);
+console.log(num1+num2+num3);
+console.log(num1+num2*num3);
+console.log((num1+num2)*num3)
+console.log(num1*num2/num3)
+/*
+incremento = ++
+decremento = --
+ */
+
+let contador = 1;
+contador++; //2
+contador++; //3
+contador++; //4
+contador++; //5
+console.log(contador);
+
+let contador1 = 10;
+console.log(--contador1);
+console.log(--contador1); 
