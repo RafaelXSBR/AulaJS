39594caf6c1ddd9b4d3f3f04febfe169a92baf93
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-03-27 14:49:47 -0300

    atualização

--- JS/aula11/js/script.js
+++ JS/aula11/js/script.js
@@ -1,4 +1,7 @@
-const num1 = prompt('Digite um numero');
-const num2 = prompt('Digite outro numero');
-const resultado = number(num1) + number(num2);
-alert(resultado);
+let num1 = prompt('Digite um numero');
+let num2 = prompt('Digite outro numero');
+
+num1=Number(num1);
+num2=Number(num2);
+ 
+alert(`o resutado da soma e ${num1 + num2}`);
