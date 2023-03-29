a2970fdee16a6fca559708b4e90e5e6b855c04d9
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-03-29 14:51:20 -0300

    edição

--- JS/aula14/index.js
+++ JS/aula14/index.js
@@ -1,4 +1,6 @@
-let num1 = 1; // number
+let num1 = 15.5555; // number
 let num2 =2.5; //number
 
-console.log(num1 + num2);
+console.log(num1.toString() + num2);
+console.log(num1.toString(2));
+console.log(num1.toFixed(2));
