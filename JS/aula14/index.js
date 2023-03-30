8c0c5f6d900e11ff9b038ae6fb4523d5150d31b3
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-03-30 13:27:50 -0300

    add conteudo

--- JS/aula14/index.js
+++ JS/aula14/index.js
@@ -1,6 +1,35 @@
-let num1 = 15.5555; // number
-let num2 =2.5; //number
+let num1 = 0.7; // number
+let num2 = 0.1; //number
 
-console.log(num1.toString() + num2);
-console.log(num1.toString(2));
-console.log(num1.toFixed(2));
+//num1 += num2; //0.8         num1 =num1 + num2
+//num1 += num2; //0.9
+//num1 += num2; //1.0
+//num1 += num2; //1.1
+//num1 += num2; //1.2
+//num1 += num2; //1.3
+//num1 += num2; //1.4
+//num1 += num2; //1.5
+//num1 += num2; //1.6
+//num1 += num2; //1.7
+//num1 += num2; //1.8
+//num1 += num2; //1.9
+//num1 += num2; //2.0
+
+//num1 = parseFloat(num1.toFixed(2));
+
+//num1 = ((num1 * 100) + (num2 * 100)) / 100; //0.8
+//num1 = ((num1 * 100) + (num2 * 100)) / 100; //0.9
+//num1 = ((num1 * 100) + (num2 * 100)) / 100; //1.0
+
+console.log(num1);
+console.log(Number.isInteger(num1));
+
+
+//console.log(num1.toString() + num2);
+//console.log(num1.toString(2));
+//console.log(num1.toFixed(2));
+//console.log(Number.isTnteger(num1));
+//let temp = num1*'casa';
+//let temp = num + '5';
+//console.log(Number.isNaN(temp));
+//console.log(temp);
