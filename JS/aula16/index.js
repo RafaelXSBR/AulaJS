ffcb241fb6e24365dea535844efb9fca9a6ec14e
Rafael Xavier Simões <rafael.xsimes@gmail.com>
2023-04-28 01:10:19 -0300

    aula 16

---
+++ JS/aula16/index.js
@@ -0,0 +1,27 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+const removido =alunos.shift(); //remove do inicio
+//const removido =alunos.pop(); //  remove do fim
+
+console.log(removido);
+console.log(alunos);
+//alunos.unshift('Luiza'); //adiciona no inicio
+//alunos.unshift('fabio');
+
+
+//alunos.push('Luiza'); // adiciona no fim
+//alunos.push('Fabio');
+
+//alunos[alunos.length]='Luiza';// adiciona no fim
+//alunos[alunos.length]='fabio';
+//alunos[alunos.length]='Luana';
+
+//console.log(alunos.length);
+
+//alunos[0]='Eduardo'; //altera
+//alunos[3]='Luiza';
+
+
+
+//console.log(alunos[0]);
+//console.log(alunos[2]);
