a8cff8ce0f39918cb95ce1576f00231d1ff55c87
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-05-23 18:53:01 -0300

    new

--- JS/tests/js/script.js
+++ JS/tests/js/script.js
@@ -2,9 +2,20 @@
 function meuEscopo (){
     const form = document.querySelector('.form');
 
-    form.onsubmit = function (evento){
-        alert(1);
+    //form.onsubmit = function (evento){
+       // evento.preventDefault();
+       // alert(1);
+      //  console.log('Foi enviado.')
         
-    };
+    //};
+    let contador = 1;
+    function recebeEventoForm (evento){
+        evento.preventDefault();
+        console.log(`Form Não foi enviado ${contador}`);
+        contador++
+    }
+
+
+    form.addEventListener('submit', recebeEventoForm);
 }
 meuEscopo();
