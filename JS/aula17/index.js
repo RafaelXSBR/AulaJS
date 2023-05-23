9563bf633ece1fa2376d7b3687adfb6ec21a5531
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-05-23 12:21:50 -0300

    new arq

---
+++ JS/aula17/index.js
@@ -0,0 +1,19 @@
+//function saudacao(nome) {
+    //console.log(`Bom dia ${nome}!`);
+    //return `Bom dia ${nome}!`;
+//}
+
+//const variavel = saudacao('Luiz');
+//console.log(variavel);
+
+//saudacao('Luiz');
+//saudacao('Maria');
+//saudacao('Felipe');
+
+function soma(x, y){
+    const resultado = x + y;
+    return resultado;
+} 
+console.log(soma(2, 2));
+console.log(soma(3, 1));
+console.log(soma(5, 10));
