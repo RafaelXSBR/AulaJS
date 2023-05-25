c6da37d5fafb3b382823e3185cd2c39e0adb039b
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-05-25 11:37:30 -0300

    new arq

---
+++ JS/aula22/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores Lógicos
+&& _> AND _> E _> Todas as expressões precisa ser verdadeiras para retornar true
+|| _> OR _> OU _> 
+! _> NOT _> NÃO
+*/
+const expressaoAnd = true && true && true && true;
+const expressaoOr = false || true;
+
+
+
+console.log(expressaoAnd);
+console.log(expressaoOr);
+console.log(!false);
