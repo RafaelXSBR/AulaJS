3193933ec8c0bf4a02d0ca157011d6c2dce08a8c
Rafael Xavier Simões <rafael.xsimoes@gmail.com>
2023-06-17 13:19:09 -0300

    new

---
+++ JS/aula27/index.js
@@ -0,0 +1,12 @@
+// ? :
+// (condição) ? 'valor para verdadeiro' : 'Valor para Falso';
+const pontuacaoUsuario = 1001;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário Vip' : 'Usuário normal'
+console.log(nivelUsuario);
+
+/*if (pontuacaoUsuario>= 1000){
+    console.log('Usuário VIP');
+}else {
+    console.log('Usuário normal');
+}
+*/
